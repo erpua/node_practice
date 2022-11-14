b0c405d12c4a55a3924053c4769ccbdf34bdfc87
Ievgen Tovtin <ievgen.tovtin.contractor@bbva.com>
2022-11-14 16:00:16 -0600

    added changes

--- LESSON01/sorting/utils/commander.js
+++ LESSON01/sorting/utils/commander.js
@@ -1,2 +1,11 @@
-const { program } = require('commander');
-program.version('0.0.1');
+/* const { commander } = require('commander');
+program.version('0.0.1');  => .   */
+
+import commander from ('commander')
+const { program } = commander
+
+export default program
+  .version('0.0.1')
+  .requiredOption('-f, --folder <type>', 'Input folder')
+  .option('-o, --output [type]', 'Output folder', './dist')
+  .option('-d, --delete', 'Delete source folder')
