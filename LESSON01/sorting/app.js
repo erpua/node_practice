a23255417bce8f95eb2ace4aced7c51afcd59060
Ievgen Tovtin <ievgen.tovtin.contractor@bbva.com>
2022-11-15 14:27:34 -0600

    added changes

--- LESSON01/sorting/app.js
+++ LESSON01/sorting/app.js
@@ -1,12 +1,36 @@
 import * as fs from 'fs/promises'
 import { isAccessible } from './utils/accessible.js'
 import { program } from './utls/commander.js'
-import { handleerror } from './utils/handleerror.js'
+import { handleError } from './utils/handleerror.js'
+import SortFiles from './module/sort.js'
 
 import { fileURLToPath } from 'url'
-import { dirname } from 'path'
+import { dirname, resolve } from 'path'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 program.parse(process.argv)
+
+/* 
+
+the version before 14 
+
+(async () => {
+  await...
+}).then(() => {})
+
+*/
+
+if(!(await isAccessible(program.output))) {
+  await fs.mkdir(program.output)
+}
+
+try {
+  const sorting = new SortFiles(program.output)
+  await sorting.readFolder(resolve(__dirname, program.folder))
+} catch (e) {
+  handleError(e)
+}
+
+console.log('Can delete source folder')
--- LESSON01/sorting/module/sort.js
+++ LESSON01/sorting/module/sort.js
@@ -1,3 +1,20 @@
-class SortFiles {}
+import * as fs from 'fs/promises'
+import { isAccessible } from './utils/accessible.js'
+import { handleError } from './utils/handleerror.js'
+import { extname } from 'path'
+
+class SortFiles {
+  constructor(dist) {
+    this.dist = dist
+  }
+
+
+  //# => private method()
+  async #copyFile(file) {
+
+  }
+
+  async readFolder(base) 
+}
 
 export default SortFiles
